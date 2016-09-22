aa918ce245079638ad5178c53f6f03eb73a0161e
Jitesh <jiteshvm@gmail.com>
2016-09-22 10:05:34 -0700

    function declarations

--- src/linkedlist.h
+++ src/linkedlist.h
@@ -5,3 +5,9 @@ struct ListNode
 	int data;
 	ListNode *next;
 };
+
+void insert_after(ListNode *node, ListNode* newnode);
+
+ListNode* search_list(ListNode *head, int key);
+
+void erase_after(ListNode *node);
